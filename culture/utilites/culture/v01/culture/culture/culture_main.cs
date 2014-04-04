23b31b728ce8b36ea536e6a3485c94aab4a52da6
palexisru <pl2-rainbow@mail.ru>
2014-04-04 14:07:15 +0400

    utilites\culture C# Expess addition

---
+++ culture/utilites/culture/v01/culture/culture/culture_main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PL2.Language.Culture
+    {
+    class Culture
+        {
+        static void Main(string[] args)
+            {
+            }
+        }
+    }
