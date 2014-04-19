b9e07d1fe63c6df0a13bc41a4ceda698c8d0f32e
palexisru <pl2-rainbow@mail.ru>
2014-04-20 02:25:02 +0400

    solution restractured

---
+++ culture/library/Culture_XML/Culture_XML/Culture_XML_main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    namespace pl2.culture
+    {
+        public class XML
+        {
+            public const file_name = "culture.xml";
+            protected readonly string path;
+            public string source;
+            public string name;
+            public string[] local(string draft)
+            {
+                return new string[0];
+            }
+            public XML(string culture_path)
+            {
+                
+                path = culture_path + "culture.xml";
+            }
+            public XML()
+            {
+                path = Environment.CurrentDirectory + "
+            }
+        }
+    }
